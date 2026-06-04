ca703adead418f42e387823e29208dc0e24b5529
geld501 <geldar.mingazov@mail.ru>
2026-06-04 10:01:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,12 +4,29 @@ const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modalWindow = document.querySelector('.sign-wind');
 
+const card = document.querySelector('.dialog-card1');
+const next1but = document.querySelector('.strel');
+
 console.log('Скрипт отработал корректно')
 
 if (openModalBtn && modalWindow) {
     console.log('2')
     openModalBtn.addEventListener("click", function(event) {
         modalWindow.removeAttribute("hidden");
+    });
+}
+
+if (card && next1but && card.getAttribute("hidden")) {
+    console.log('5')
+    next1but.addEventListener("click", function(event) {
+        card.removeAttribute("hidden");
+    });
+}
+
+if (card && next1but && !card.getAttribute("hidden")) {
+    console.log('6')
+    next1but.addEventListener("click", function(event) {
+        card.setAttribute("hidden");
     });
 }
 
